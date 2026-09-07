ccfa6ff3a29a0afb763c54a4a4fb82ae187bc0e0
AnatoliyHaymin <91140911+AnatoliyHaymin@users.noreply.github.com>
2026-09-07 13:18:58 +0300

    Init
    Add VIsual Studio project

---
+++ TextPipeline/Domain/Contracts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    //public record OperationContract(...
+    //...
+    //...
+    //);
+
+
+    public static class Contracts
+    {
+
+    }
+}
